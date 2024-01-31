ee420abfda2bc57b60a925d891216ac387f86e8f
Samarin Aleksei <liotbiu1@gmail.com>
2024-01-31 17:25:55 +0100

    up padding

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { CheckTable } from "@/components/component/check-table";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between lg:p-24">
       <h2 className="text-2xl font-bold mb-4">April 2023 Public Data File from Crossref</h2>
       <CheckTable></CheckTable>
     </main>
